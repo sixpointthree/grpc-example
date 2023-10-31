4ba17bc58fa92ba0132fe0bbfce948258ffc228b
Steffen Lang <steffenlang1994@gmail.com>
2023-10-31 14:32:29 +0000

    Revert "failed try for creating a CMake package"
    This reverts commit 61478b10b70b0da1b2d72d6eb4d01364be52773a.

--- src/order_handler_lib_cpp/build/OrderHandler-config.cmake
+++
@@ -1,9 +0,0 @@
-# Compute installation prefix relative to this file.
-get_filename_component(_dir "${CMAKE_CURRENT_LIST_FILE}" PATH)
-get_filename_component(_prefix "${_dir}/../.." ABSOLUTE)
-
-# Import the targets.
-include("${_prefix}/lib/OrderHandler-1.0/OrderHandler-targets.cmake")
-
-# Report other information.
-set(OrderHandler_INCLUDE_DIRS "${_prefix}/include/OrderHandler-1.0")
